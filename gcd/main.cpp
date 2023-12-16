1e2ec930ed487b8c12166505f5b86b81bf3c6f5a
kedaya-blue <52734343+kedaya-blue@users.noreply.github.com>
2023-12-16 10:34:27 +0800

    Add files via upload

---
+++ gcd/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "iostream"
+#include "vector"
+
+using namespace std;
+int main()
+{
+	long long int m, n,c;
+	cout << "请输入" << endl;
+	cin >> m >> n;
+	while (cin.fail() || cin.bad() || m == 0||n==0) {
+		cout << "输入不合法，请重新输入" << endl;
+		cin >> m >> n;
+	}
+	int divisor[100000]{};
+	c = m + n;
+		//vector<vector<vector<int>>>divisor(m, vector < vector<int>>(m, vector<int>(m,0)));
+		if (divisor) {
+			for (int i = 0; i <c; i++) {
+				if (m % (static_cast<long long>(i) + 1) == 0 && n % (static_cast<long long>(i) + 1) == 0) {
+					divisor[i] = 1;
+				}
+				else
+					divisor[i] = 0;
+			}
+			while (m > 0) {
+				if (divisor[m-1] == 1) {
+					cout << "最大公约数为" << m << endl;
+					break;
+				}
+				else
+					m--;
+
+			}
+		}
+	
+}
