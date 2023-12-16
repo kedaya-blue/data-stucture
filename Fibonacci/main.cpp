c58b21ca494348f881f5273e9c559aaba2c094ab
kedaya-blue <52734343+kedaya-blue@users.noreply.github.com>
2023-12-16 10:33:05 +0800

    Add files via upload

---
+++ Fibonacci/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "iostream"
+
+using namespace std;
+static long long int Fibonacci(int n) {
+	if (n < 1)
+		return 0;
+	else if (n == 1)
+		return 1;
+	return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+int main()
+{
+	int n;
+	cin >> n;
+	cout<< Fibonacci(n);
+}
