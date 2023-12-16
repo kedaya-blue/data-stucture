e52b49b22df4f1d0e5530b6cf06a5961f6bf7fe1
kedaya-blue <52734343+kedaya-blue@users.noreply.github.com>
2023-12-16 10:37:13 +0800

    Add files via upload
    找出中间值

---
+++ middlenum/main.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int a[3]{};
+	int n;
+	cout << "请输入三个数" << endl;
+	cin.sync();
+	for (int i = 0; i<3; i++) {
+		cin >> a[i];
+		while (1) {
+			if (cin.fail()||cin.bad()) {
+				cout << "输入不合法，请重新输入" << endl;
+			}
+			else
+				break;
+			cin.clear();
+			cin.ignore();
+			cin >> a[i];
+		}
+	}
+	for (int i = 0; i<3; i ++) {
+		for (int j = 0; j<3; j ++) {
+			if (a[i] >= a[j]) {
+				n = a[i];
+				a[i] = a[j];
+				a[j] = n;
+			}
+		}
+	}
+	cout << a[1];
+
+}
