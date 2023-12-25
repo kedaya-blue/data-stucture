86a0d91edb52018cf01789eb7ec1c29945a093d9
kedaya-blue <52734343+kedaya-blue@users.noreply.github.com>
2023-12-25 08:55:19 +0800

    Add files via upload

---
+++ 输出图案/main.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "iostream"
+
+using namespace std;
+static void output(int n) {
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+static void output1(int n) {
+	for (int i = 0; i <= n; i++) {
+		for (int j = n; j >i; j--) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	output(n);
+	output1(n);
+}
