cb40b13ad3c3f484a4c8a5ce04423f5d266112b4
kedaya-blue <52734343+kedaya-blue@users.noreply.github.com>
2023-12-25 08:54:02 +0800

    Add files via upload
    求多个数之和

---
+++ 多个数之和/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "iostream"
+#include "math.h"
+#include "vector"
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 0;
+	cout << "请输入要输入的数的个数" << endl;
+	cin >> n;
+	while (cin.bad() || cin.fail() || n <= 0) {
+		cout << "输入不合法，请重新输入" << endl;
+		cin >> n;
+	}
+	vector<int>arr(n,0);
+	cout << "请输入数字" << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+	cout << "总和为：" << sum << endl;
+}
